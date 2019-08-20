43111c78d5f6707a7abb1dd9e4ccd4a6c1d8a8c2
Cristo Sparks <crisparks@hotmail.com>
2019-08-20 01:45:30 -0500

    Created the online Spring Boot Project
    This was created by following the "Getting Stated on Spring Boot Project" guide that was supplied by Merit America.

---
+++ Prototype/onlinebank/src/main/java/com/meritamerica/onlinebank/MeritAmericaOnlineBankApplication.java
@@ -0,0 +1,13 @@
+package com.meritamerica.onlinebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeritAmericaOnlineBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeritAmericaOnlineBankApplication.class, args);
+	}
+
+}
